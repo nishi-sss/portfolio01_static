705f102da4d7a8e36b5c8f8eba96f4ea9c553dd5
nishi <return.0return.1@gmail.com>
2025-02-03 23:19:28 +0900

    yml変更

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -18,7 +18,9 @@ def get_photos():
 
     try:
         response = s3.list_objects_v2(Bucket=bucket_name)
+        print("S3 Response:", response)
         for obj in response.get('Contents', []):
+            print("Object key:", obj)
             photo_url = f"https://{bucket_name}.s3.amazonaws.com/{obj['Key']}"
             photos.append(photo_url)
     except Exception as e:
